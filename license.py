793dfcf257cee244e0a243ca781e24b0aa85e9a7
Mark Boleigha <donhoenix@gmail.com>
2025-07-21 14:28:05 +0100

    remove licensing

--- license.py
+++ license.py
@@ -11,32 +11,33 @@ class License:
 		return
 
 	def check_license(self):
-	    try:
-	        # Get the system timezone
-	        local_tz = str(tzl.get_localzone()).replace(" ", "_")
-	        time_server_url = f"https://worldtimeapi.org/api/timezone/{local_tz}"
+		return "OK"
+	    # try:
+	    #     # Get the system timezone
+	    #     local_tz = str(tzl.get_localzone()).replace(" ", "_")
+	    #     time_server_url = f"https://worldtimeapi.org/api/timezone/{local_tz}"
 
-	        response = requests.get(time_server_url)
-	        response.raise_for_status()
-	        server_time = datetime.fromisoformat(response.json()['utc_datetime'].replace("Z", "+00:00"))
-	        system_time = datetime.now(pytz.utc)
+	    #     response = requests.get(time_server_url)
+	    #     response.raise_for_status()
+	    #     server_time = datetime.fromisoformat(response.json()['utc_datetime'].replace("Z", "+00:00"))
+	    #     system_time = datetime.now(pytz.utc)
 
-	        time_difference = abs((server_time - system_time).total_seconds())
-	        if time_difference > 60 or time_difference < -60:
-	            return "Your system time is not in sync. Please update your date and time settings."
-	            # return False
+	    #     time_difference = abs((server_time - system_time).total_seconds())
+	    #     if time_difference > 60 or time_difference < -60:
+	    #         return "Your system time is not in sync. Please update your date and time settings."
+	    #         # return False
 
-	        # Replace '2024-10-08' with your desired cutoff date
-	        # 
-	        cutoff_date = datetime.fromisoformat("2024-10-13T10:42:52.168310+00:00")
-	        if system_time > cutoff_date:
-	            return "This product is expired, please purchase a live copy"
-	            # return False
+	    #     # Replace '2024-10-08' with your desired cutoff date
+	    #     # 
+	    #     cutoff_date = datetime.fromisoformat("2024-10-13T10:42:52.168310+00:00")
+	    #     if system_time > cutoff_date:
+	    #         return "This product is expired, please purchase a live copy"
+	    #         # return False
 
-	        return "OK"
+	    #     return "OK"
 
-	    except requests.exceptions.RequestException as e:
-	        # return False
-	        return "Please check your internet connection or firewall."
+	    # except requests.exceptions.RequestException as e:
+	    #     # return False
+	    #     return "Please check your internet connection or firewall."
 
 
